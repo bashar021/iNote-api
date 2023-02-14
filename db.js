99118b0df3619fb3a81ebc7a2530cf41113c99c2
bashar021 <alambashar021@gmail.com>
2023-02-14 17:19:24 +0530

    version 1.3

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 //connecting db
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+var mongoose = require('mongoose');
+var {Schema} = mongoose;
 function db(){
+  mongoose.set('strictQuery', false);
   mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected!'))
   .catch(err=> console.log(err));
